7535ac83a2a6a5b894af216dc2018c074d15ccc5
Isa <137991239+isaf123@users.noreply.github.com>
2024-10-21 08:54:49 +0700

    add logo first menu

---
+++ src/app/(root)/menu/page.tsx
@@ -0,0 +1,68 @@
+"use client";
+import ButtonMorph from "@/components/ButtonMorph";
+import Link from "next/link";
+
+// import Daftarkan from "@/assets/icons/daftarkan.svg";
+export default function Home() {
+  const content = [
+    {
+      click: "Pengertian Akta Cerai, Salinan Putusan, dan Salinan Penetapan ",
+      link: "/informasi",
+    },
+    {
+      click:
+        "Persyaratan Pengambilan Produk Pengadilan \n (Akta Cerai, Salinan Putusan, dan Salinan Penetapan)",
+      link: "/",
+    },
+    {
+      click: "Informasi Tarif Penerimaan Bukan Pajak (PNBP) Produk Pengadilan",
+      link: "/hargatarif",
+    },
+    {
+      click: "Sistem Booking Online Pengambilan Produk Pengadilan",
+      link: "/daftar",
+    },
+    {
+      click:
+        "Pengisian Formulir Khusus pengambilan Produk Pengadilan melalui Kuasa Hukum/ Kuasa Insidentil",
+      link: "https://docs.google.com/forms/d/e/1FAIpQLSfbOZ-myoypA8yDnrNcYOqlwNvGuA140XeVJJE7p28w3wZxkA/viewform?usp=sf_link",
+    },
+  ];
+
+  return (
+    <div className="font-[family-name:var(--font-geist-sans)] bg-black text-gray-900">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start relative">
+        <div className="relative w-full h-0 pb-[56.25%]">
+          <div
+            className="absolute top-0 left-0 w-full h-full bg-cover bg-center"
+            style={{
+              backgroundImage: "url('/images/paasampang.png')",
+            }}
+          >
+            <div className="absolute inset-0 bg-gradient-to-t from-black via-black/60 to-transparent z-0"></div>
+            <div className="absolute top-1/2 left-1/2  -translate-x-1/2 -translate-y-1/2 max-md:-translate-y-10">
+              <div className="w-fit m-auto min-h-screen flex items-center flex-col gap-5 justify-center ">
+                <h3 className="max-md:text-2xl text-5xl max-md:w-[320px] w-[700px] text-white text-center mb-10">
+                  Layanan Pengurusan{" "}
+                  <span className="font-bold text-[#a9efc3] italic">
+                    Akta Cerai
+                  </span>{" "}
+                  Pengadilan Agama Sampang
+                </h3>
+                {content.map(
+                  (item: { link: string; click: string }, idx: number) => {
+                    return (
+                      <Link href={item.link} key={idx}>
+                        <ButtonMorph>{item.click}</ButtonMorph>
+                      </Link>
+                    );
+                  }
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,34 +1,12 @@
-"use client";
-import ButtonMorph from "@/components/ButtonMorph";
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+import { Separator } from "@/components/ui/separator";
+import { MoveRight } from "lucide-react";
+import BlurFade from "@/components/ui/blur-fade";
 import Link from "next/link";
 
 // import Daftarkan from "@/assets/icons/daftarkan.svg";
 export default function Home() {
-  const content = [
-    {
-      click: "Pengertian Akta Cerai, Salinan Putusan, dan Salinan Penetapan ",
-      link: "/informasi",
-    },
-    {
-      click:
-        "Persyaratan Pengambilan Produk Pengadilan \n (Akta Cerai, Salinan Putusan, dan Salinan Penetapan)",
-      link: "/",
-    },
-    {
-      click: "Informasi Tarif Penerimaan Bukan Pajak (PNBP) Produk Pengadilan",
-      link: "/hargatarif",
-    },
-    {
-      click: "Sistem Booking Online Pengambilan Produk Pengadilan",
-      link: "/daftar",
-    },
-    {
-      click:
-        "Pengisian Formulir Khusus pengambilan Produk Pengadilan melalui Kuasa Hukum/ Kuasa Insidentil",
-      link: "https://docs.google.com/forms/d/e/1FAIpQLSfbOZ-myoypA8yDnrNcYOqlwNvGuA140XeVJJE7p28w3wZxkA/viewform?usp=sf_link",
-    },
-  ];
-
   return (
     <div className="font-[family-name:var(--font-geist-sans)] bg-black text-gray-900">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start relative">
@@ -41,23 +19,114 @@ export default function Home() {
           >
             <div className="absolute inset-0 bg-gradient-to-t from-black via-black/60 to-transparent z-0"></div>
             <div className="absolute top-1/2 left-1/2  -translate-x-1/2 -translate-y-1/2 max-md:-translate-y-10">
-              <div className="w-fit m-auto min-h-screen flex items-center flex-col gap-5 justify-center ">
-                <h3 className="max-md:text-2xl text-5xl max-md:w-[320px] w-[700px] text-white text-center mb-10">
-                  Layanan Pengurusan{" "}
-                  <span className="font-bold text-[#a9efc3] italic">
-                    Akta Cerai
-                  </span>{" "}
-                  Pengadilan Agama Sampang
-                </h3>
-                {content.map(
-                  (item: { link: string; click: string }, idx: number) => {
-                    return (
-                      <Link href={item.link} key={idx}>
-                        <ButtonMorph>{item.click}</ButtonMorph>
-                      </Link>
-                    );
-                  }
-                )}
+              <div className="w-fit m-auto min-h-screen flex items-center flex-col gap-5 justify-center max-md:mt-10 mt-[600px]">
+                <BlurFade delay={0.25} inView>
+                  <div className="flex flex-col items-center">
+                    <Image
+                      src={"/images/logosirakyat.png"}
+                      alt=""
+                      width={500}
+                      height={300}
+                      className="max-md:w-[320px] max-md:h-fit"
+                    />
+                    <p className="text-white text-sm">
+                      <span className="text-[#a9efc3]">Oleh :</span> Tamara
+                    </p>
+                    <Link href={"/menu"}>
+                      <Button className="bg-[#a9efc3] text-black mt-5 mb-10 hover:text-white">
+                        lanjut
+                        <MoveRight className="size-4 ml-2" />
+                      </Button>
+                    </Link>
+                  </div>
+                </BlurFade>
+                <BlurFade delay={0.25 * 2} inView>
+                  <div className="text-white max-md:text-sm text-xl max-md:w-screen w-[550px] flex flex-col gap-5">
+                    <div className="flex gap-6  items-center px-4">
+                      <h3 className="text-[#a9efc3] text-4xl font-semibold">
+                        SI
+                      </h3>
+                      <p>Sistem Informasi</p>
+                    </div>
+                    <Separator className="opacity-30" />
+                    <div className="flex gap-8  items-center px-4">
+                      <h3 className="text-[#a9efc3] text-4xl font-semibold">
+                        R
+                      </h3>
+                      <p>
+                        sosialisasi masya
+                        <span className="font-extrabold text-[#a9efc3]">R</span>
+                        akat untuk pentingnya layananan digital di era
+                        digitalisasi
+                      </p>
+                    </div>
+
+                    <Separator className="opacity-30" />
+                    <div className="flex gap-8  items-center px-4">
+                      <h3 className="text-[#a9efc3] text-4xl font-semibold">
+                        A
+                      </h3>
+                      <p>
+                        melakukan kolaborasi dengan{" "}
+                        <span className="font-extrabold text-[#a9efc3]">A</span>
+                        hli IT bidang coding
+                      </p>
+                    </div>
+                    <Separator className="opacity-30" />
+
+                    <div className="flex gap-8  items-center px-4">
+                      <h3 className="text-[#a9efc3] text-4xl font-semibold">
+                        K
+                      </h3>
+                      <p>
+                        membuat rancangan isi web dalam bentu
+                        <span className="font-extrabold text-[#a9efc3]">
+                          K
+                        </span>{" "}
+                        scan barcode
+                      </p>
+                    </div>
+                    <Separator className="opacity-30" />
+
+                    <div className="flex gap-8  items-center px-4">
+                      <h3 className="text-[#a9efc3] text-4xl font-semibold">
+                        Y
+                      </h3>
+                      <p>
+                        mensinkronkan data dalam pen
+                        <span className="font-extrabold text-[#a9efc3]">Y</span>
+                        usunan isi web{" "}
+                      </p>
+                    </div>
+                    <Separator className="opacity-30" />
+
+                    <div className="flex gap-8  items-center px-4">
+                      <h3 className="text-[#a9efc3] text-4xl font-semibold">
+                        A
+                      </h3>
+                      <p>
+                        membuat notifik
+                        <span className="font-extrabold text-[#a9efc3]">A</span>
+                        si kepada para pihak pencari keadilan didalam web
+                        layanan penyerahan produk pengadilan{" "}
+                      </p>
+                    </div>
+                    <Separator className="opacity-30" />
+
+                    <div className="flex gap-8  items-center px-4">
+                      <h3 className="text-[#a9efc3] text-4xl font-semibold">
+                        T
+                      </h3>
+                      <p>
+                        membuat notifik membuat formulir iden
+                        <span className="font-extrabold text-[#a9efc3]">T</span>
+                        itas diri dalam penyerahan surat kuasa secara digital
+                        dalam web e-akta cerai
+                      </p>
+                    </div>
+                    <Separator className="opacity-30" />
+                  </div>
+                </BlurFade>
               </div>
             </div>
           </div>
